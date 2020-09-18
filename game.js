9861c5c038ed0a3fd0e84b673fd82bf01b6e7866
Gavin-Jacobs <gavin@phoenixvir.net>
2020-09-19 00:58:06 +1200

    did

--- game.js
+++ game.js
@@ -41,7 +41,9 @@ function updateCounts () {
   }
   
   // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
-
+  makeGreen.length
+  makeBlue.length
+  hide.length
   // Once you've done the counting, this function will update the display
   displayTotals(totals)
 }
